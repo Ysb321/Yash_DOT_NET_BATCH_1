282bf6cb5b222b278d74db45b6ddc55de4b2240e
ysb321 <ysb1571@gmail.com>
2024-08-29 22:40:36 +0530

    First Project

--- AssignMent1/Program.cs
+++ AssignMent1/Program.cs
@@ -1,1 +1,49 @@
-﻿using system;
+﻿using System;
+
+public class Rectangle
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+   
+    public double CalculateArea()
+    {
+        return length * width;
+    }
+}
+
+public class Square
+{
+    private double side;
+
+    
+    public Square(double side)
+    {
+        this.side = side;
+    }
+
+        public double CalculateArea()
+    {
+        return side * side;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Rectangle rect = new Rectangle(15, 13);
+        Console.WriteLine("Area of the rectangle: " + rect.CalculateArea());
+
+        
+        Square sq = new Square(5);
+        Console.WriteLine("Area of the square: " + sq.CalculateArea());
+    }
+}
